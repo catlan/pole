97aebef90af739c881b07a6fbb9c1fa3093f609f
Ariya Hidayat <ariya@sencha.com>
2007-04-11 13:11:45 +0000

    relicense under two-clause BSD license, this is the only variant allowed for KDE libraries

--- poledump.cpp
+++ poledump.cpp
@@ -1,29 +1,26 @@
-/* POLE - Portable library to access OLE Storage 
-   Copyright (C) 2002-2005 Ariya Hidayat <ariya@kde.org>
+/*  POLE - Portable C++ library to access OLE Storage
+    Copyright (C) 2002-2007 Ariya Hidayat (ariya@kde.org).
 
-   Redistribution and use in source and binary forms, with or without 
-   modification, are permitted provided that the following conditions 
-   are met:
-   * Redistributions of source code must retain the above copyright notice, 
-     this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright notice, 
-     this list of conditions and the following disclaimer in the documentation 
-     and/or other materials provided with the distribution.
-   * Neither the name of the authors nor the names of its contributors may be 
-     used to endorse or promote products derived from this software without 
-     specific prior written permission.
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
 
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
-   THE POSSIBILITY OF SUCH DAMAGE.
+    1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <iostream>
@@ -51,11 +48,11 @@ void visit( int indent, POLE::Storage* storage, std::string path )
     if( ss ) if( !ss->fail() )std::cout << "  (" << ss->size() << ")";
     std::cout << std::endl;
     delete ss;
-    
+
     if( storage->isDirectory( fullname ) )
       visit( indent+1, storage, fullname + "/" );
   }
-  
+
 }
 
 void dump( POLE::Storage* storage, char* stream_name )
@@ -63,7 +60,7 @@ void dump( POLE::Storage* storage, char* stream_name )
   POLE::Stream* stream = new POLE::Stream( storage, stream_name );
   if( !stream ) return;
   if( stream->fail() ) return;
-  
+
   // std::cout << "Size: " << stream->size() << " bytes" << std::endl;
   unsigned char buffer[16];
   for( ;; )
@@ -74,10 +71,10 @@ void dump( POLE::Storage* storage, char* stream_name )
       std::cout << "    ";
       for( unsigned i = 0; i < read; i++ )
         printf( "%c", ((buffer[i]>=32)&&(buffer[i]<128)) ? buffer[i] : '.' );
-      std::cout << std::endl;      
+      std::cout << std::endl;
       if( read < sizeof( buffer ) ) break;
   }
-  
+
   delete stream;
 }
 
@@ -86,10 +83,10 @@ void extract( POLE::Storage* storage, char* stream_name, char* outfile )
   POLE::Stream* stream = new POLE::Stream( storage, stream_name );
   if( !stream ) return;
   if( stream->fail() ) return;
-  
+
   std::ofstream file;
   file.open( outfile, std::ios::binary|std::ios::out );
-  
+
   unsigned char buffer[16];
   for( ;; )
   {
@@ -98,7 +95,7 @@ void extract( POLE::Storage* storage, char* stream_name, char* outfile )
       if( read < sizeof( buffer ) ) break;
   }
   file.close();
-  
+
   delete stream;
 }
 
@@ -110,7 +107,7 @@ int main(int argc, char *argv[])
     std::cout << argv[0] << " filename [stream-name [output-file]]" << std::endl;
     return 0;
   }
-  
+
   char* filename = argv[1];
   char* streamname = (argc<3) ? 0 : argv[2];
   char* outfile = (argc<4) ? 0 : argv[3];
@@ -122,15 +119,15 @@ int main(int argc, char *argv[])
     std::cout << "Error on file " << filename << std::endl;
     return 1;
   }
-  
+
   if( !streamname )
     visit( 0, storage, "/" );
   else if( !outfile )
      dump( storage, streamname );
   else
      extract( storage, streamname, outfile );
-    
+
   delete storage;
-  
+
   return 0;
 }
